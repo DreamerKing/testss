156329dfdf44f4657853bf75e6f051b1b5e945a0
Dreamer King <dreamingking@live.cn>
2022-07-06 10:32:53 +0800

    chore: 入口文件改写

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import { createRoot } from 'react-dom';
 import App from './app';
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
 
